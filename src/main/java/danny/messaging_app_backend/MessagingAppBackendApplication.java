69229bf195f757f62a2ffe1635bcf63febd61182
Danny Garcia <dgarcia1724@berkeley.edu>
2024-11-14 00:16:53 -0800

    Database connected

---
+++ src/main/java/danny/messaging_app_backend/MessagingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package danny.messaging_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danny/messaging_app_backend/MessagingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danny.messaging_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
